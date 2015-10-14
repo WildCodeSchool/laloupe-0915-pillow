4c4522ab2672bafa48b95ad5d2caffbe7543d76c
naamelie <nadalini.amelie@gmail.com>
2015-10-14 15:42:56 +0200

    Before the raking fucking command

--- db/migrate/20151014131917_create_apparts.rb
+++ db/migrate/20151014131917_create_apparts.rb
@@ -1,7 +1,19 @@
 class CreateApparts < ActiveRecord::Migration
   def change
     create_table :apparts do |t|
-
+      t.boolean :room_type
+      t.string :property_type
+      t.integer :accommodates
+      t.string :city
+      t.string :title
+      t.integer :bedroom_nb
+      t.integer :beds_nb
+      t.float :price
+      t.string :adress
+      t.integer :zip_code
+      t.string :country
+      t.string :amenities
+      t.string :images
       t.timestamps null: false
     end
   end
