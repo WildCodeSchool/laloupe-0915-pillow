177bbfb9211a71fd486a0b981ed103127ca728e9
VictorGoutay <victor.goutay@gmail.com>
2015-10-15 15:50:34 +0200

    Push for ludo

--- app/controllers/apparts_controller.rb
+++ app/controllers/apparts_controller.rb
@@ -2,4 +2,7 @@ class AppartsController < ApplicationController
   def index
     @apparts = Appart.all
   end
+  def bien
+    @appart = Appart.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   root "apparts#index"
+  get 'appart/:id' => 'apparts#bien'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
