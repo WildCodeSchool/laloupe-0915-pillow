3a198099fc3e8a4b9f5ef06fd8aa4c44bd553a2a
ludivinecp <coipel.ludivine@gmail.com>
2015-10-20 17:01:16 +0200

    user profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   #before_action :set_home, only: [:show, :edit, :update, :destroy]
+
+  def edit 
+  	@user = User.new
+  end
+
+  def index
+  end
+
+  def create
+  	if @user.save
+     redirect_to user_path
+   else
+     render :new
+  	end
+  end
+
+  def show
+    @users = User.find(params[:id])
+    render "devise/confirmations/show"
+  end
+
+  def update
+  	render "devise/confirmations/update"
+  	@users = User.find(params[:id])
+  end
+
 end
