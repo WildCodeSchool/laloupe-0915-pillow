e9b80dce5260a927d667a6e4c45fc97d07f59d33
naamelie <nadalini.amelie@gmail.com>
2015-10-22 15:43:03 +0200

    Resolve migration pb

--- db/migrate/20151021141339_add_userid_to_appart.rb
+++
@@ -1,6 +0,0 @@
-class AddUseridToAppart < ActiveRecord::Migration
-  def change
-
-  	add_reference :apparts, :user, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20151021141339) do
+
+ActiveRecord::Schema.define(version: 20151021120915) do
 
   create_table "apparts", force: :cascade do |t|
     t.boolean  "room_type"
